3e1d040959bd1a50692a15dca0a090d4374c37e9
Rifqy Hazim H R <rifqyhazim22@gmail.com>
2025-11-07 14:02:56 +0700

    fix(api): import auth module for payments guard

--- apps/api/src/modules/payments/payments.module.ts
+++ apps/api/src/modules/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentsController } from "./payments.controller";
 import { PaymentsService } from "./payments.service";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { PaymentsWebhookController } from "./webhook.controller";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [PaymentsController, PaymentsWebhookController],
   providers: [PaymentsService],
   exports: [PaymentsService],
